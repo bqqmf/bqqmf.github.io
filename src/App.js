7be338c9f2b10f4281b66abde9a6ef58cf5428f2
JeongHwan Oh <wjdghks728@naver.com>
2025-08-25 21:02:30 +0900

    [Update] Router -> HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import SmartDesign from "./SmartDesign";
 import CapstoneDesign from "./CapstoneDesign";
@@ -8,13 +8,13 @@ import "./style.css";
 
 export default function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/smart-design" element={<SmartDesign />}></Route>
         <Route path="/capstone-design" element={<CapstoneDesign />}></Route>
         <Route path="/UX-design" element={<UXDesign />}></Route>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
