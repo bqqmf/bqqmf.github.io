ffbe2d44eb0d1e086aa48b213b4c020322718bd5
JeongHwan Oh <wjdghks728@naver.com>
2025-10-07 21:28:20 +0900

    img grayscale and Main img full

--- src/Main.js
+++ src/Main.js
@@ -3,16 +3,25 @@ import "./Main.css";
 
 export default function Main() {
   return (
-    <div className="main">
-      <img src="images/main_page.gif" alt="main page" style={{ width: "1440px", height: "972px" }} />
-       <span className="main-text"> 휘광 <br/>輝光 </span>
-      <Link to="/home">
-        <button className="enter-btn">ENTER</button>
-      </Link>
+    <div style={{ width: "100%", height: "100%", position: "relative" }}>
+      <img
+        src="images/main_page.gif"
+        alt="main page"
+        className="background-img"
+      />
+
+      <div className="center-content">
+        <span className="main-text"> 휘광 <br />輝光 </span>
+        <Link to="/home">
+          <button className="enter-btn">ENTER</button>
+        </Link>
+      </div>
+
       <span className="bottom-text">
         인덕대학교 디지털산업디자인학과 2025학년도 졸업전시 <br />
         Induk University Digital Industrial Design Department 2025 Graduation Exhibition
       </span>
     </div>
+
   );
 }
